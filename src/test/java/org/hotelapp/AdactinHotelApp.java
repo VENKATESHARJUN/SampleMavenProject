80d00e69f684a7b469bef6e4b6c0d1676999b246
VENKATESHARJUN <rarjun.tech@gmail.com>
2020-12-18 22:51:08 +0530

    Task Completed

---
+++ src/test/java/org/hotelapp/AdactinHotelApp.java
@@ -0,0 +1,10 @@
+package org.hotelapp;
+
+public class AdactinHotelApp {
+	
+	
+public static void main(String[] args) {
+	System.out.println("Im Arjun");
+}
+
+}
---
+++ src/test/java/org/hotelapp/BaseClass.java
@@ -0,0 +1,5 @@
+package org.hotelapp;
+
+public class BaseClass {
+
+}
