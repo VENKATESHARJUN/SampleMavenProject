22d0ed0eb9e73c847538fe83c3ebfc3e1705153d
VENKATESHARJUN <rarjun.tech@gmail.com>
2020-12-19 07:50:51 +0530

    HotelApp updated code

--- src/test/java/org/hotelapp/AdactinHotelApp.java
+++ src/test/java/org/hotelapp/AdactinHotelApp.java
@@ -4,7 +4,9 @@ public class AdactinHotelApp {
 	
 	
 public static void main(String[] args) {
-	System.out.println("Im Arjun");
+	System.out.println("New Sample2 >> Java");
+	System.out.println("New Sample2 >> python");
+	System.out.println("New Sample2 >> ruby");
 }
 
 }
