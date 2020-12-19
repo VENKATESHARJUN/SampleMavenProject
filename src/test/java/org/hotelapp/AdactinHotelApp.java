5af1cdd2966ca332de103efb07b6c6f10904532b
VENKATESHARJUN <rarjun.tech@gmail.com>
2020-12-19 08:36:19 +0530

    ArjunRv Newly Updated the Code

--- src/test/java/org/hotelapp/AdactinHotelApp.java
+++ src/test/java/org/hotelapp/AdactinHotelApp.java
@@ -4,9 +4,18 @@ public class AdactinHotelApp {
 	
 	
 public static void main(String[] args) {
+	
+	// NewSample2 project Code
 	System.out.println("New Sample2 >> Java");
 	System.out.println("New Sample2 >> python");
 	System.out.println("New Sample2 >> ruby");
+	
+	// ArjunRv project Code
+	System.out.println("ArjunRv >> Chennai");
+	System.out.println("ArjunRv >> Manargudi");
+	System.out.println("ArjunRv >> Needamnagalam");
+	
+	
 }
 
 }
